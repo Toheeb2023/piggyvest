1a4fe05ef76ce47b91079bdf44e7e6f0a7337236
Toheeb2023 <omotoshotoheeb2022@gmail.com>
2023-06-21 13:29:12 -1200

    nav update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const hamburger = document.querySelector('.hamburger');
+// const bar = document.querySelectorAll('.bar');
 const links = document.querySelector('.links');
 const cols = document.querySelector('.show-links');
 
@@ -7,6 +8,9 @@ hamburger.addEventListener('click', () => {
     links.classList.toggle('active'); 
 })
     
+document.querySelector('.bar').forEach(n => n.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+}))
 
 document.querySelectorAll('.li').forEach(n => n.addEventListener('click', () => {
     hamburger.classList.remove('active')
