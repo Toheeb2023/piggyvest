de1ac55ecd78a4fda1bd46d6c7dcf5653c770d98
Toheeb2023 <omotoshotoheeb2022@gmail.com>
2023-06-22 16:15:12 -1200

    navbar fixed

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ const hamburger = document.querySelector('.hamburger');
 // const bar = document.querySelectorAll('.bar');
 const links = document.querySelector('.links');
 const cols = document.querySelector('.show-links');
+const body = document.body;
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     links.classList.toggle('active'); 
+    body.classList.toggle('overflow');
 })
     
 document.querySelector('.bar').forEach(n => n.addEventListener('click', () => {
     hamburger.classList.toggle('active');
+    body.classList.toggle('overflow');
 }))
 
 document.querySelectorAll('.li').forEach(n => n.addEventListener('click', () => {
     hamburger.classList.remove('active')
     links.classList.remove('active')
+    body.classList.remove('overflow');
 }))
